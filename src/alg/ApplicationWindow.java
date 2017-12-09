01ef87221c1025a2c163b53554658f2562885b10
Melroy van Nijnatten <m.v.nijnatten@student.tue.nl>
2017-12-10 00:48:37 +0100

    [/fuckit] Starting over. Completely.

--- src/alg/ApplicationWindow.java
+++ src/alg/ApplicationWindow.java
@@ -6,86 +6,13 @@ import java.awt.event.*;
 
 public class ApplicationWindow {
     private JPanel rootPanel;
-    private JPanel contentPanel;
-    private JPanel statusBar;
-    private JPanel topBar;
-    private JLabel mousePositionLabel;
-    private JButton resetButton;
-    private JButton endTurnButton;
-    private JCheckBox drawTriangulationCheckBox;
-    private JCheckBox drawCircumcirclesCheckBox;
-    private JCheckBox drawDebugOverlayCheckBox;
-
-    // The current state of the game and the manager of the game.
-    private final GameManager manager;
 
     private ApplicationWindow() {
-        // Initialize the game manager.
-        manager = new GameManager(this);
-        ((GamePanel) contentPanel).setManager(manager);
-
         // Initialize the listeners we use.
         initializeListeners();
-
-
-
     }
 
     private void initializeListeners() {
-        contentPanel.addMouseMotionListener(new MouseMotionAdapter() {
-            @Override
-            public void mouseMoved(MouseEvent e) {
-                super.mouseMoved(e);
-                mousePositionLabel.setText("Cursor position: (" + e.getX() + ", " + e.getY() + ")");
-            }
-        });
-
-        contentPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                super.mousePressed(e);
-
-                // Depending on the mouse button, execute an action.
-                switch (e.getButton()) {
-                    case MouseEvent.BUTTON1:
-                        manager.addPoint(e.getPoint());
-                        break;
-//                    case MouseEvent.BUTTON3:
-//                        manager.removePoint(e.getPoint());
-                }
-
-                // Since we made an update to the canvas, repaint.
-                contentPanel.repaint();
-            }
-        });
-
-        resetButton.addActionListener(e -> {
-            // Reset the game state and the manager.
-            manager.reset();
-
-            // Since we made an update to the canvas, repaint.
-            contentPanel.repaint();
-        });
-
-        endTurnButton.addActionListener(e -> {
-            // Switch the currently active player in the game state.
-            manager.switchPlayer();
-        });
-
-        drawTriangulationCheckBox.addActionListener(e -> {
-            ((GamePanel) contentPanel).drawTriangulation = drawTriangulationCheckBox.isSelected();
-            contentPanel.repaint();
-        });
-
-        drawCircumcirclesCheckBox.addActionListener(e -> {
-            ((GamePanel) contentPanel).drawCircumcircles = drawCircumcirclesCheckBox.isSelected();
-            contentPanel.repaint();
-        });
-
-        drawDebugOverlayCheckBox.addActionListener(e -> {
-            ((GamePanel) contentPanel).drawDebugOverlay = drawDebugOverlayCheckBox.isSelected();
-            contentPanel.repaint();
-        });
 
     }
 
@@ -97,23 +24,13 @@ public class ApplicationWindow {
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setSize(screenSize);
+        frame.setPreferredSize(screenSize);
+        frame.setMinimumSize(screenSize);
         frame.pack();
         frame.setVisible(true);
-//        frame.setResizable(false);
-
-//        frame.setPreferredSize(new Dimension(1000, 700));
-//        frame.pack();
-//        frame.setLocationByPlatform(true);
-//        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(ApplicationWindow::createAndShowGui);
-    }
-
-    private void createUIComponents() {
-        // Create the content panel object.
-        contentPanel = new GamePanel();
     }
 }
