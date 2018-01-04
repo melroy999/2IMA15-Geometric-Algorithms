faf6793f6bd970f943e915c380f9f3e9186a05cc
George Park <george--park@hotmail.com>
2018-01-04 14:51:52 +0100

    Minor changes in RandomAI and started work on GridAI
    Minor changes in RandomAI, mostly comments and restructuring as well as changing Move class as we no longer need to remove points. Started work on GridAI, scaffolding and created form for GridAI

--- src/geo/player/GridAIPlayer.java
+++ src/geo/player/GridAIPlayer.java
@@ -7,68 +7,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-public class RandomAIPlayer extends AIPlayer {
+public class GridAIPlayer extends AIPlayer  {
+    private JPanel rootPanel;
+    private JTextField numPoints;
+
     // A list of a list of moves to do.
     private List<List<Move>> moves;
 
     // The current index of the sublist we are iterating over.
     private int sublistid = 0;
 
-    private boolean duplicate;
-
-    private JPanel rootPanel;
-    private JTextField seed;
-    private JTextField numPoints;
-
     /**
      * Create a player, given the game controller to communicate with.
      *
      * @param controller The game controller to communicate with.
-     * @param player The human player we should follow up with when the AI is done.
-     * @param turn The turn this player should be active in.
+     * @param player     The human player we should follow up with when the AI is done.
+     * @param turn       The turn this player should be active in.
      */
-    public RandomAIPlayer(GameController controller, HumanPlayer player, GameState.PlayerTurn turn){
+    public GridAIPlayer(GameController controller, HumanPlayer player, GameState.PlayerTurn turn) {
         super(controller, player, turn);
     }
 
     /**
-     * Create the list of random moves for the AI to take.
+     * Create the list of moves in a grid pattern for the AI to take.
      *
-     * @param seed The seed with which to generate the random numbers.
      * @param numPoints The number of points to be placed.
      */
-    private void generateRandomMoves(long seed, int numPoints){
-        // Create the list of moves.
+    public void createGrid(int numPoints){
+        // TODO: Create a grid
         moves = new ArrayList<>();
         moves.add(new ArrayList<>());
-        Random generator = new Random(seed);
-        int i = 0;
-        // While there are not enough points, generate new random coordinates and try to add them
-        while (i < numPoints) {
-            int x = generator.nextInt(100/*Limit of x playing field, get width of panel*/);
-            int y = generator.nextInt(100/*Limit of y playing field, get height of panel*/);
-            System.out.println(x);
-            // Reset duplicate value and j value
-            duplicate = false;
-            int j = 0;
-            // Check if the point already exists in moves
-            for(Move p : moves.get(j)){
-                // If x and y coordinates exists in list of moves, ignore and generate new x and y
-                if (p.p.x == x && p.p.y == y) {
-                    duplicate = true;
-                }
-                j++;
-            }
-            // If the random point is not duplicate, add the point to the list of moves and increase list size.
-            if (!duplicate) {
-                moves.get(moves.size() - 1).add(new Move(new Point(x, y)));
-                i++;
-            }
-        }
     }
-
 
     /**
      * Run the AI of this player.
@@ -77,14 +47,10 @@ public class RandomAIPlayer extends AIPlayer {
      */
     @Override
     protected void runAI(GameState state) {
-        // Get the values for seed and numPoints
-        long seedValue = Long.parseLong(seed.getText());
+        // Get the value for numPoints
         int numPointsValue = Integer.parseInt(numPoints.getText());
-        // Create the list of moves to make
-        generateRandomMoves(seedValue, numPointsValue);
-        // For test purposes, remove later
-        Move pointx = moves.get(0).get(0);
-        System.out.println(pointx.p.x);
+        // Create the list of moves
+        createGrid(numPointsValue);
 
         // Iterate over the moves, making them.
         for(Move p : moves.get(sublistid)) {
@@ -97,9 +63,6 @@ public class RandomAIPlayer extends AIPlayer {
                 e.printStackTrace();
             }
         }
-
-        // Increment the sublist id.
-        sublistid++;
     }
 
     /**
@@ -109,7 +72,7 @@ public class RandomAIPlayer extends AIPlayer {
      */
     @Override
     public boolean isDone() {
-        return !(sublistid < moves.size());
+        return false;
     }
 
     /**
@@ -117,7 +80,7 @@ public class RandomAIPlayer extends AIPlayer {
      */
     @Override
     public void reset() {
-        sublistid = 0;
+
     }
 
     /**
@@ -129,9 +92,6 @@ public class RandomAIPlayer extends AIPlayer {
     public JPanel getPanel() {
         return rootPanel;
     }
-
-
-
 
     /**
      * A class that manages the addition of moves.
