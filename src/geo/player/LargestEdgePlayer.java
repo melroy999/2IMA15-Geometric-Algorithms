9831a995886b5103220153b33d8d47fbe4e19c9c
mauritsambags <mauritsambags@gmail.com>
2018-01-18 14:11:10 +0100

    [/Greedy-AI] LargestEdgePlayer seems to work fine now. Added some filtering on the allowed edges per move to fix illegal behaviour.

--- src/geo/player/LargestEdgePlayer.java
+++ src/geo/player/LargestEdgePlayer.java
@@ -2,9 +2,11 @@ package geo.player;
 
 import geo.controller.GameController;
 import geo.delaunay.TriangleFace;
+import geo.gui.GUI;
 import geo.state.GameState;
 import geo.store.halfedge.Edge;
 import geo.store.halfedge.Vertex;
+import geo.store.math.Vector2d;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import java.util.Comparator;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.Point;
+import java.awt.Dimension;
 
 public class LargestEdgePlayer extends AIPlayer {
 
@@ -24,6 +27,8 @@ public class LargestEdgePlayer extends AIPlayer {
     private int turn = 0;
     //Number of failed moves.
     private int failedMoves = 0;
+    //List with the Edges that we have used before (so we do not re-use them in later turns).
+    List<Edge> visitedEdges = new ArrayList();
 
     public LargestEdgePlayer(GameController controller, HumanPlayer player, GameState.PlayerTurn turn){
         super(controller, player, turn);
@@ -66,6 +71,8 @@ public class LargestEdgePlayer extends AIPlayer {
         GameState.FaultStatus status = addPoint(centre);
 
         if (status == GameState.FaultStatus.None){
+            visitedEdges.add(largestEdge);
+            visitedEdges.add(largestEdge.twin);
             turn++;
         }   else {
             System.out.println(status);
@@ -88,11 +95,37 @@ public class LargestEdgePlayer extends AIPlayer {
         //Find the largest Edge in this list.
         return edges
                 .stream()
+                .filter(a -> isValidEdge(a))
                 .max(Comparator.comparingDouble(a -> a.origin.distance(a.twin.origin))).get();
     }
 
+    /**
+     * Function to determine whether this edge is actually located on the gameboard or has already been used.
+     * (thus this edge is not one of the edges placed at infinity).
+     * @param e Edge to evaluate.
+     * @return whether this edge is actually entirely on the gameboard.
+     */
+    private boolean isValidEdge(Edge e){
+        //If this edge has the same start/end as an already used edge, do not use it.
+        //Cannot use id's for this part since the random construction may re-assign id's between moves (it seems).
+        if (visitedEdges.stream()
+                .anyMatch(a -> a.origin.x == e.origin.x
+                        && a.origin.y == e.origin.y && a.twin.origin.x == e.twin.origin.x
+                        && a.twin.origin.y == e.twin.origin.y)) return false;
+
+        Vertex start = e.origin;
+        Vertex end = e.twin.origin;
+        Dimension boardSize = GUI.createAndShow().getGamePanelDimensions();
+
+        //Check if this edge's start and end are within bounds.
+        return 0 <= start.x && start.x <= boardSize.width
+                && 0 <= end.x && end.x <= boardSize.width
+                && 0 <= start.y && start.y <= boardSize.height
+                && 0 <= end.y && end.y <= boardSize.height;
+    }
+
     @Override
-    public void reset(){ turn = 0; failedMoves = 0; isDone = false;}
+    public void reset(){ turn = 0; failedMoves = 0; isDone = false; visitedEdges.clear();}
 
     @Override
     public boolean isRandom(){ return false; }
